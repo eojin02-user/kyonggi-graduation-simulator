aa97476bb3de697273c95f6bd7907c42c7b49646
eojin02-user <eojin02@gmail.com>
2026-07-13 17:45:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('Kyonggi Graduation Simulator Beta 0.1');
+const tbody=document.querySelector('#tbl tbody');
+function render(list){tbody.innerHTML='';list.forEach(c=>{tbody.innerHTML+=`<tr><td>${c[0]}</td><td>${c[1]}</td><td>${c[2]}</td><td>${c[3]}</td><td>${c[4]}</td></tr>`})}
+function filterCourses(){let q=document.getElementById('search').value.toLowerCase();render(courses.filter(c=>c[0].toLowerCase().includes(q)));}
+render(courses);
