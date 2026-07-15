e05b8d9f39103d3d75d1c0f08a0e5fff56713464
eojin02-user <eojin02@gmail.com>
2026-07-15 23:11:03 +0900

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const tbody=document.querySelector('#tbl tbody');
-function render(list){tbody.innerHTML='';list.forEach(c=>{tbody.innerHTML+=`<tr><td>${c[0]}</td><td>${c[1]}</td><td>${c[2]}</td><td>${c[3]}</td><td>${c[4]}</td></tr>`})}
-function filterCourses(){let q=document.getElementById('search').value.toLowerCase();render(courses.filter(c=>c[0].toLowerCase().includes(q)));}
-render(courses);
